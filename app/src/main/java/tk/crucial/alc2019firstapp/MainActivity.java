acb1cf9c5b64677d87491c0175e539b9b8f950cb
Nicholas <nich.otieno@gmail.com>
2019-05-08 20:20:46 +0300

    Add Java Code

--- app/src/main/java/tk/crucial/alc2019firstapp/MainActivity.java
+++ app/src/main/java/tk/crucial/alc2019firstapp/MainActivity.java
@@ -1,5 +1,6 @@
 package tk.crucial.alc2019firstapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,7 +24,13 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                TextView textValue = findViewById(R.id.text_value);
+                String stringValue = textValue.getText().toString();
+                int originalValue = Integer.parseInt(stringValue);
+                int newValue = MyWorker.doubleTheValue(originalValue);
+                textValue.setText(String.format("%s",newValue));
+
+                Snackbar.make(view, "Changed Value " + originalValue + " to " + newValue, Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
---
+++ app/src/main/java/tk/crucial/alc2019firstapp/MyWorker.java
@@ -0,0 +1,7 @@
+package tk.crucial.alc2019firstapp;
+
+class MyWorker {
+    static int doubleTheValue(int value){
+        return value * 2;
+    }
+}
